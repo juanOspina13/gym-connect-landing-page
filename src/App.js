b184ec0acd0bfd56d508a514cfe1d8e96e07abf9
juanOspina13 <jumaos1989@gmail.com>
2022-02-18 14:35:18 -0500

    se quita imagen

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './assets/logo.png'
+/* import logo from './assets/logo.png'*/ 
 import './App.css'
 
 function App() {
@@ -6,7 +6,9 @@ function App() {
     <div className="App">
       <header></header>
       <div className="App-main">
+        {/*
         <img className="App-main__gc-logo" src={logo} alt="logo"></img>
+         */}
         <p>Pagina Gym Connect</p>
       </div>
     </div>
