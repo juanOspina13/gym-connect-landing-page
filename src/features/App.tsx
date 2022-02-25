b9e3cce2b79bc583afdae3b0055f0a99040cdf71
Juan Ospina <juan.ospinac@perficient.com>
2022-02-25 10:43:56 -0500

    uso styledComponents

--- src/features/App.tsx
+++ src/features/App.tsx
@@ -1,13 +1,25 @@
+/* import logo from './assets/logo.png'*/ 
+import styled from 'styled-components';
+import './App.css'
+
+const WelcomeMessageComponent = styled.header`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 10px 20px;
+  color:#cee741;
+`;
+
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-         Pagina de Gym Connect
-        </p>
-      </header>
+      <header></header>
+      <div className="App-main">
+        <WelcomeMessageComponent>Pagina Gym Connect</WelcomeMessageComponent>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
