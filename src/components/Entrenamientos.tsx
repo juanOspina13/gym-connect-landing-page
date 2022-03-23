c720ed00495f798ce938f22303f44272ac64e730
juanOspina13 <jumaos1989@gmail.com>
2022-03-22 20:27:22 -0500

    layout general

--- src/components/Entrenamientos.tsx
+++ src/components/Entrenamientos.tsx
@@ -1,5 +1,53 @@
+import { useState } from "react";
+
+interface IDiaEntreno {
+    fondo: string;
+    titulo: string;
+}
 const Entrenamientos = () => {
-    return (<>Entrenamientos</>)
+    const [entrenos, setEntrenos] = useState([
+        {
+            fondo: '../assets/lunes.png',
+            titulo: 'lunes',
+            id: 'lunes',
+        },
+        {
+            fondo: '../assets/martes.png',
+            titulo: 'martes',
+            id: 'martes',
+        },
+        {
+            fondo: '../assets/miercoles.png',
+            titulo: 'miercoles',
+            id: 'miercoles',
+        },
+        {
+            fondo: '../assets/jueves.png',
+            titulo: 'jueves',
+            id: 'jueves',
+        },
+        {
+            fondo: '../assets/viernes.png',
+            titulo: 'viernes',
+            id: 'viernes',
+        },
+        {
+            fondo: '../assets/sabado.png',
+            titulo: 'sabado',
+            id: 'sabado',
+        }
+    ])
+    return (<div className="entrenamientos">
+        <h1 className="title">
+            Entrenamientos</h1>
+        <div className="dia-entreno-container flex gap-4">
+            {entrenos.map(entreno => {
+                return (<div className="dia-entreno flex w-1/6" key={entreno.id}>
+                    {entreno.titulo}
+                </div>)
+            })}
+        </div>
+    </div>)
 }
 
 export default Entrenamientos;
--- src/components/MainStyles.ts
+++ src/components/MainStyles.ts
@@ -7,11 +7,16 @@ color: white;
 height: 100vh;
 display: flex;
 flex-direction: column;
+gap:3rem;
+
 `;
 export const MainContainer = styled.div`
 justify-content: center;
 background-color: black;
 color: white;
 display: flex;
+align-items: center;
+flex-direction: column;
+gap:3rem;
 align-items: center;
 `;
