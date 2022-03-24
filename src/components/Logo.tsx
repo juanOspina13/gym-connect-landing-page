e333e684650d7543e701aa64f1c3ac25596db977
juanOspina13 <jumaos1989@gmail.com>
2022-03-23 20:22:37 -0500

    ajustes sizing y espacios

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -5,14 +5,14 @@ import { Parallax } from "react-scroll-parallax";
 const Logo = () => {
   return (
     <div className="h-screen flex flex-col justify-center header-content">
-     <Parallax className="flex flex-col  justify-center items-center" speed={-20}>
-           <img className="max-w-[120px]" src={LogoHeader} alt="logoHeader" />
-      <img
-        className="max-w-sm md:max-w-screen-sm max-h-[100px] w-full"
-        src={GymConnectHeader}
-        alt="gymConnectHeader"
-      />
-          </Parallax>
+      <Parallax className="flex flex-col gap-8 justify-center items-center" speed={-20}>
+        <img className="max-w-[162px]" src={LogoHeader} alt="logoHeader" />
+        <img
+          className="max-w-sm md:max-w-screen-md max-h-[100px] w-full"
+          src={GymConnectHeader}
+          alt="gymConnectHeader"
+        />
+      </Parallax>
 
     </div>
   );
